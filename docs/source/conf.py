a51a6a8deff4fad78e7ebbf6a8aa82fb3b832de9
Raphaël Ribes <raphael.ribes@gmail.com>
2023-10-25 16:48:13 +0200

    ???

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RaphUtils'
+copyright = '2023, Ribes'
+author = 'Raphaël Ribes'
 
 release = '0.1'
 version = '0.1.0'
